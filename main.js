be18565f31cad353566ab84427a7feb9f0a68b33
Amaan7979 <88380417+Amaan7979@users.noreply.github.com>
2021-08-12 17:23:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,27 @@ function preload() {
 
 }
 function setup() {
-canvas= createCanvas(400,400);
+canvas= createCanvas(400,350);
 canvas.center();
-
+video=createCapture(VIDEO);
+video.hide();
+}
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/OfAbBeokz/model.json",modelLoaded);
+function draw() {
+    image(video,0,0,400,350);
+    classifier.classify(video,got_result);
+}
+console.log("ml5 version",ml5.version);
+function modelLoaded() {
+    console.log("Model is Loaded");
+}
+function got_result(error,results) {
+    if(error) {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("object_result").innerHTML=results[0].label;
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(2);
+    }
 }
